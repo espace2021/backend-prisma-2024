2fb792b14d9a3dff3d3232b5c6f27f3ea39f0862
sh <sandrahammamitlili@gmail.com>
2024-02-09 13:36:31 +0100

    bonjour-commit

--- app.js
+++ app.js
@@ -22,3 +22,5 @@ app.use('/api/articles', articlesRouter);
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
+module.exports = app;
